7edc538b00b080279e51c39c817e582b6f340b8f
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-02-09 22:20:43 -0500

    FIXES: No duplicate props allowed

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -165,7 +165,6 @@ const CoffeeStore = (initialProps) => {
             <h1 className={styles.name}>{name}</h1>
           </div>
           <Image
-            alt="banner image"
             src={
               imgUrl ||
               "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
