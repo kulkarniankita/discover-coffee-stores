89b2fc7f4f751e184dd3dbd7e7a4c92f71d27b1d
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-05-29 21:49:54 -0400

    _app.js Solution with Footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer>
+        <p>© 2021 Ankita</p>
+      </footer>
+    </div>
+  );
 }
 
 export default MyApp;
