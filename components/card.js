87cd8e3bcf0894da26becc9ed4960a9ccc436ae9
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-05-30 00:31:53 -0400

    Card Component Styling: styles for card

--- components/card.js
+++ components/card.js
@@ -1,11 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./card.module.css";
 
 const Card = (props) => {
   return (
-    <Link href={props.imgUrl}>
-      <a>
-        <h2>{props.name}</h2>
-        <Image src={props.imgUrl} width={260} height={160} />
+    <Link href={props.href}>
+      <a className={styles.cardLink}>
+        <div className={styles.container}>
+          <div className={styles.cardHeaderWrapper}>
+            <h2 className={styles.cardHeader}>{props.name}</h2>
+          </div>
+          <div className={styles.cardImageWrapper}>
+            <Image
+              className={styles.cardImage}
+              src={props.imgUrl}
+              width={260}
+              height={160}
+            />
+          </div>
+        </div>
       </a>
     </Link>
   );
