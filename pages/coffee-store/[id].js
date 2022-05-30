d1437bc5516652d3e686b2a8fbb493cb8c0cf6f4
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-05-29 22:58:47 -0400

    Add route using Link component: Add link back to home

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,9 +1,17 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CoffeeStore = () => {
   const router = useRouter();
   console.log("router", router);
-  return <div>Coffee Store Page {router.query.id}</div>;
+  return (
+    <div>
+      Coffee Store Page {router.query.id}
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </div>
+  );
 };
 
 export default CoffeeStore;
