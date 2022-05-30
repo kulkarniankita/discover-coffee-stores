b71e57e16239f2f1af4a74360a524e762feecca7
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-05-29 22:16:57 -0400

    Banner Component: Simple Banner component

---
+++ components/banner.js
@@ -0,0 +1,5 @@
+const Banner = () => {
+  return <div>Banner</div>;
+};
+
+export default Banner;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
+
+import Banner from "../components/banner";
 
 export default function Home() {
   console.log("styles", styles);
@@ -14,6 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Coffee Connoisseur</h1>
+        <Banner />
       </main>
     </div>
   );
