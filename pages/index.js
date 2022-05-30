07181d9f8389cc7e10e3774093bdbd7cac08f5c3
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-05-30 01:00:39 -0400

    Add Coffee Stores on Page: use dummy coffee store data

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import styles from "../styles/Home.module.css";
 import Banner from "../components/banner";
 import Card from "../components/card";
 
+import coffeeStores from "../data/coffee-stores.json";
+
 export default function Home() {
   const handleOnBannerBtnClick = () => {
     console.log("hi banner button");
@@ -26,18 +28,16 @@ export default function Home() {
           <Image src="/static/hero-image.png" width={700} height={400} />
         </div>
         <div className={styles.cardLayout}>
-          <Card
-            name="DarkHorse Coffee"
-            imgUrl="/static/hero-image.png"
-            href="/coffee-store/darkhose-coffee"
-            className={styles.card}
-          />
-          <Card
-            name="DarkHorse Coffee"
-            imgUrl="/static/hero-image.png"
-            href="/coffee-store/darkhose-coffee"
-            className={styles.card}
-          />
+          {coffeeStores.map((coffeeStore) => {
+            return (
+              <Card
+                name={coffeeStore.name}
+                imgUrl="/static/hero-image.png"
+                href="/coffee-store/darkhose-coffee"
+                className={styles.card}
+              />
+            );
+          })}
         </div>
       </main>
     </div>
