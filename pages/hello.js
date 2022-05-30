08d6108ec62e1bf7e52df146888aadfc1ef70ac8
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-05-29 22:07:07 -0400

    Let's get ready for Home Page: remove hello page + style

--- pages/hello.js
+++
@@ -1,8 +0,0 @@
-import styles from "../styles/Hello.module.css";
-
-const Hello = () => {
-  console.log("styles for Hello", styles);
-  return <div className={styles.container}>Hello World</div>;
-};
-
-export default Hello;
