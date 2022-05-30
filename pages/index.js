17138d21f5132b56416d3264f8c301fb002f2597
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-05-29 22:13:00 -0400

    App background: footer removed

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,6 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Coffee Connoisseur</h1>
       </main>
-
-      <footer className={styles.footer}></footer>
     </div>
   );
 }
