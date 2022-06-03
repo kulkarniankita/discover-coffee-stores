5b770fdda3d6b18b7185c252566d3515afad2475
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-03 17:22:09 -0400

    Fallback: true

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -18,13 +18,17 @@ export async function getStaticProps(staticProps) {
 export function getStaticPaths() {
   return {
     paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
-    fallback: false,
+    fallback: true,
   };
 }
 
 const CoffeeStore = (props) => {
   const router = useRouter();
   console.log("router", router);
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
 
   console.log("props", props);
   return (
