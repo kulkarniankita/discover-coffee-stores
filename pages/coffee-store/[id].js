97ca5ceb8d12dfea9df710b1f7028dfc22ed0837
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-03 16:50:46 -0400

    Implement Get Static Props and Get Static Paths in Next.js

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,6 +1,25 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+export async function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id === 0; //dynamic id
+      }),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+  };
+}
+
 const CoffeeStore = () => {
   const router = useRouter();
   console.log("router", router);
