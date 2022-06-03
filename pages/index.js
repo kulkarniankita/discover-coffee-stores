ab569138a1a3f661c05a1928f4bf72876fad66a1
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-03 18:47:13 -0400

    Foursquare API in Get Static Props

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,23 @@ import coffeeStoresData from "../data/coffee-stores.json";
 
 export async function getStaticProps(context) {
   console.log("hi getStaticProps");
+
+  const options = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: "<add your foursquare token here>",
+    },
+  };
+
+  fetch(
+    "https://api.foursquare.com/v3/places/search?query=coffee&ll=43.653833032607096%2C-79.37896808855945&limit=6",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+
   return {
     props: {
       coffeeStores: coffeeStoresData,
