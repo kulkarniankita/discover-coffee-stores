d07490d3b3adcf083c5bd3b009c339a5a75bd5b1
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-03 18:17:53 -0400

    Refactor Coffee Store Page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
 
 import coffeeStoresData from "../../data/coffee-stores.json";
 
@@ -16,8 +17,15 @@ export async function getStaticProps(staticProps) {
 }
 
 export function getStaticPaths() {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
   return {
-    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    paths,
     fallback: true,
   };
 }
@@ -30,18 +38,20 @@ const CoffeeStore = (props) => {
     return <div>Loading...</div>;
   }
 
+  const { address, name, neighbourhood } = props.coffeeStore;
+
   console.log("props", props);
   return (
     <div>
-      Coffee Store Page {router.query.id}
+      <Head>
+        <title>{name}</title>
+      </Head>
       <Link href="/">
         <a>Back to home</a>
       </Link>
-      <Link href="/coffee-store/dynamic">
-        <a>Go to page dynamic</a>
-      </Link>
-      <p>{props.coffeeStore.address}</p>
-      <p>{props.coffeeStore.name}</p>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighbourhood}</p>
     </div>
   );
 };
