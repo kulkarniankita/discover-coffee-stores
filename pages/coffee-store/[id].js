d2e5987441ea1551e367ac68eed88bc6a6fb4d59
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-03 18:25:47 -0400

    Coffee Store Page Styling

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,8 +1,11 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Head from "next/head";
+import Image from "next/image";
 
 import coffeeStoresData from "../../data/coffee-stores.json";
+
+import styles from "../../styles/coffee-store.module.css";
 
 export async function getStaticProps(staticProps) {
   const params = staticProps.params;
@@ -32,26 +35,41 @@ export function getStaticPaths() {
 
 const CoffeeStore = (props) => {
   const router = useRouter();
-  console.log("router", router);
-
   if (router.isFallback) {
     return <div>Loading...</div>;
   }
 
-  const { address, name, neighbourhood } = props.coffeeStore;
+  const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
 
-  console.log("props", props);
   return (
-    <div>
+    <div className={styles.layout}>
       <Head>
         <title>{name}</title>
       </Head>
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
-      <p>{address}</p>
-      <p>{name}</p>
-      <p>{neighbourhood}</p>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>Back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={imgUrl}
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
+        </div>
+
+        <div className={styles.col2}>
+          <p>{address}</p>
+          <p>{neighbourhood}</p>
+        </div>
+      </div>
     </div>
   );
 };
