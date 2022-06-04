6454e949001da76a9d05d25982815780f9f635f1
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-04 18:17:36 -0400

    Update Styling for the App

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -58,7 +58,7 @@ const CoffeeStore = (props) => {
         <div className={styles.col1}>
           <div className={styles.backToHomeLink}>
             <Link href="/">
-              <a>Back to home</a>
+              <a>← Back to home</a>
             </Link>
           </div>
           <div className={styles.nameWrapper}>
