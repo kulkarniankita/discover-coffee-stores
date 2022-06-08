f20a20b09368535d06bee1049a6221e7bb9f39c5
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-08 00:49:15 -0400

    Find Coffee Store API

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -7,12 +7,25 @@ const table = base("coffee-stores");
 
 console.log({ table });
 
-const createCoffeeStore = (req, res) => {
+const createCoffeeStore = async (req, res) => {
   console.log({ req });
   if (req.method === "POST") {
-    res.json({ message: "Hi there" });
-  } else {
-    res.json({ message: "method is GET" });
+    //find a record
+
+    const findCoffeeStoreRecords = await table
+      .select({
+        filterByFormula: `id="0"`,
+      })
+      .firstPage();
+
+    console.log({ findCoffeeStoreRecords });
+
+    if (findCoffeeStoreRecords.length !== 0) {
+      res.json(findCoffeeStoreRecords);
+    } else {
+      //create a record
+      res.json({ message: "create a record" });
+    }
   }
 };
 
