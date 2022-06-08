7f7219420b50e01f8b0889c54076edc7250d8624
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-07 23:47:04 -0400

    Hello World API route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log({ req, res });
+  res.status(200).json({ name: "John Doe" });
 }
