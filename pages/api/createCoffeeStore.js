e65d481c72c49d224184cc50a4bf81c8f06c71a4
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-08 00:20:04 -0400

    Airtable Library

---
+++ pages/api/createCoffeeStore.js
@@ -0,0 +1,6 @@
+const Airtable = require("airtable");
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE_KEY
+);
+
+const table = base("coffee-stores");
