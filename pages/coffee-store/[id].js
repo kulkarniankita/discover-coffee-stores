4c8850affc8c7808cb8a416d5465c55d9f4fb6ea
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-07 23:09:45 -0400

    Fallback in Next.js

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -6,7 +6,6 @@ import Image from "next/image";
 import cls from "classnames";
 
 import styles from "../../styles/coffee-store.module.css";
-
 import { fetchCoffeeStores } from "../../lib/coffee-stores";
 
 export async function getStaticProps(staticProps) {
@@ -14,11 +13,12 @@ export async function getStaticProps(staticProps) {
   console.log("params", params);
 
   const coffeeStores = await fetchCoffeeStores();
+  const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
+    return coffeeStore.id.toString() === params.id; //dynamic id
+  });
   return {
     props: {
-      coffeeStore: coffeeStores.find((coffeeStore) => {
-        return coffeeStore.id.toString() === params.id; //dynamic id
-      }),
+      coffeeStore: findCoffeeStoreById ? findCoffeeStoreById : {},
     },
   };
 }
