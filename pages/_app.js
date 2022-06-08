a53aaff734e084442109f672826269e2c621fec9
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-07 23:17:23 -0400

    Create Store Context

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
+import { createContext } from "react";
 import "../styles/globals.css";
+
+const StoreContext = createContext();
+
+const StoreProvider = ({ children }) => {
+  const initialState = {
+    latLong: "",
+    coffeeStores: [],
+  };
+  return (
+    <StoreContext.Provider value={{ state: initialState }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <StoreProvider>
       <Component {...pageProps} />
-    </div>
+    </StoreProvider>
   );
 }
 
