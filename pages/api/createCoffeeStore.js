206bad729bd66a5841bc4c63305a8ee1e714cac1
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-08 23:26:46 -0400

    Refactor Coffee Store By Id API

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,4 +1,8 @@
-import { table, getMinifiedRecords } from "../../lib/airtable";
+import {
+  table,
+  getMinifiedRecords,
+  findRecordByFilter,
+} from "../../lib/airtable";
 
 const createCoffeeStore = async (req, res) => {
   if (req.method === "POST") {
@@ -8,14 +12,9 @@ const createCoffeeStore = async (req, res) => {
 
     try {
       if (id) {
-        const findCoffeeStoreRecords = await table
-          .select({
-            filterByFormula: `id="${id}"`,
-          })
-          .firstPage();
+        const records = await findRecordByFilter(id);
 
-        if (findCoffeeStoreRecords.length !== 0) {
-          const records = getMinifiedRecords(findCoffeeStoreRecords);
+        if (records.length !== 0) {
           res.json(records);
         } else {
           //create a record
--- pages/api/getCoffeeStoreById.js
+++ pages/api/getCoffeeStoreById.js
@@ -1,18 +1,13 @@
-import { table, getMinifiedRecords } from "../../lib/airtable";
+import { findRecordByFilter } from "../../lib/airtable";
 
 const getCoffeeStoreById = async (req, res) => {
   const { id } = req.query;
 
   try {
     if (id) {
-      const findCoffeeStoreRecords = await table
-        .select({
-          filterByFormula: `id="${id}"`,
-        })
-        .firstPage();
+      const records = await findRecordByFilter(id);
 
-      if (findCoffeeStoreRecords.length !== 0) {
-        const records = getMinifiedRecords(findCoffeeStoreRecords);
+      if (records.length !== 0) {
         res.json(records);
       } else {
         res.json({ message: `id could not be found` });
