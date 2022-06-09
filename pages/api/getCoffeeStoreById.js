f38b9bb486e2f5cb011051836507415b7f32dfce
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-08 23:24:39 -0400

    Coffee Store By Id API Validation

--- pages/api/getCoffeeStoreById.js
+++ pages/api/getCoffeeStoreById.js
@@ -1,9 +1,22 @@
+import { table, getMinifiedRecords } from "../../lib/airtable";
+
 const getCoffeeStoreById = async (req, res) => {
   const { id } = req.query;
 
   try {
     if (id) {
-      res.json({ message: `Id is created ${id}` });
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id="${id}"`,
+        })
+        .firstPage();
+
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = getMinifiedRecords(findCoffeeStoreRecords);
+        res.json(records);
+      } else {
+        res.json({ message: `id could not be found` });
+      }
     } else {
       res.status(400);
       res.json({ message: "Id is missing" });
