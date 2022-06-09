20fee614e8c379eb052d1717a7b3c25850333ec2
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-06-09 00:02:13 -0400

    Favourite Coffee Store By Id API

---
+++ pages/api/favouriteCoffeeStoreById.js
@@ -0,0 +1,27 @@
+import { findRecordByFilter } from "../../lib/airtable";
+
+const favouriteCoffeeStoreById = async (req, res) => {
+  if (req.method === "PUT") {
+    try {
+      const { id } = req.body;
+
+      if (id) {
+        const records = await findRecordByFilter(id);
+
+        if (records.length !== 0) {
+          res.json(records);
+        } else {
+          res.json({ message: "Coffee store id doesn't exist", id });
+        }
+      } else {
+        res.status(400);
+        res.json({ message: "Id is missing" });
+      }
+    } catch (error) {
+      res.status(500);
+      res.json({ message: "Error upvoting coffee store", error });
+    }
+  }
+};
+
+export default favouriteCoffeeStoreById;
